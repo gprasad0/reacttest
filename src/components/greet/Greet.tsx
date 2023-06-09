ed566a9f6efc190187d3179dcee0997b9a284564
gprasad0 <guru96392@gmail.com>
2023-06-09 11:27:05 +0530

    changes

--- src/components/greet/Greet.tsx
+++ src/components/greet/Greet.tsx
@@ -1,3 +1,3 @@
 export const Greet = () => {
-  return <div>Hello</div>;
+  return <div>hello</div>;
 };
--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,3 +1,8 @@
+import {render,screen} from "@testing-library/react"
+import {Greet} from './Greet';
 test('Greet renders correctly',()=>{
-    
+    render(<Greet />)
+    let textElement = screen.getByText(/hello/i)
+    expect(textElement).toBeInTheDocument()
+
 })
